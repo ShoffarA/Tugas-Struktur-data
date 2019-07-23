57992813e0e057dd9af1664ef40baf390d5a5a3e
shoffar <53028687+shoffar@users.noreply.github.com>
2019-07-23 16:51:31 +0700

    Membuat program penambahan bilangan ARRAY menggunakan bahasa python
    Tugas Membuat program penambahan bilangan array menggunakan bahasa python

---
+++ class array.py
@@ -0,0 +1,10 @@
+x = [5, 20, 30]
+y = [20, 30, 60]
+
+xTotal = sum(x)
+yTotal = sum (y)
+jumlah = xTotal + yTotal
+print("Jumlah Nilai Kedua Array:", jumlah)
+
+print("Gabungan Array")
+print(x + y)
