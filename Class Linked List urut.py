64d5461476fe5ea8011b398bdedf09bc67a7d9ab
shoffar <53028687+shoffar@users.noreply.github.com>
2019-07-24 14:02:07 +0700

    Membuat Program Aplikasi Llinked List urut Menggunakan Bahasa Python

---
+++ Class Linked List urut.py
@@ -0,0 +1,12 @@
+list = [5,8,6,7,9,10,13,12,11]
+
+print("Mengecek List Awal")
+print(list)
+
+print("Menambahkan Data List dengan Insert")
+list.insert(2,70)
+print(list)
+
+print("Mengurutkan List")
+list.sort()
+print(list)
